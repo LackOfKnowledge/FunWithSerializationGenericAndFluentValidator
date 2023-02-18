5f534a5d812de66b30da1b60c9ae74276c43c155
Bolko <95961186+LackOfKnowledge@users.noreply.github.com>
2023-02-18 11:36:37 +0100

    Dodaj pliki projektów.

---
+++ Models/BaseSerializer.cs
@@ -0,0 +1,12 @@
+﻿namespace FunWithSerialization.Models
+{
+    public abstract class BaseSerializer
+    {
+        protected string xmlFilePath = @"Data\Person.xml";
+        protected string xsdFilePath = @"Data\Person.xsd";
+        public BaseSerializer()
+        {
+
+        }
+    }
+}
